1e3a350e05f6880ba90a8fa892250daaeba3c44a
Maia Sason <mayasasson2000@gmail.com>
2020-12-08 17:50:59 -0500

    Connect to sqlAlchemy postgress db

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
+# import model of db from models.py
+from models import *
+
 #import everything from wtform_fields
 from wtform_fields import *
 
 # Configure App
 #wsgei there has to be central callable obj
 app = Flask(__name__)
+
+# Configure db
+# We need to tell flask the location of db we will access and mod
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://tqhtmhdvlrvncj:409dab96337fd78dea74bf6d1367818c04f803b89e5f614a2e89c729b9ad83e5@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d8gh9apsp8ul1n'
+db = SQLAlchemy(app)
 
 # create secret key to keep client session secure, cookies during sess
 app.secret_key = 'replace'
@@ -23,8 +31,21 @@ def index():
     # else return false
     reg_form.validate_on_submit()
     if reg_form.validate_on_submit():
-        return "Success!"
-  
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        #  check if username exists
+        user_object = User.query.filter_by(username=username).first()
+        
+        if user_object:
+            return "Username taken"
+        
+        # Add user
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted to DB"
+
 
     # when installing flask, it installs Jinja
     # reusable templates for web app.
---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """ User Model """
+    #  without giving a name, class sqlalchemy will
+    #  take name of class as table
+
+    #  To access table:
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+
